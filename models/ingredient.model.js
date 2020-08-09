66bcb84c8f3c3d2c1c0d0f64130a4d1afa250c85
Maeva Merrou <maeva.merrou@live.fr>
2020-08-09 14:44:09 +0200

    update

--- models/ingredient.model.js
+++ models/ingredient.model.js
@@ -28,5 +28,35 @@ const ingredientSchema = new mongoose.Schema({
     }   
 )    
 
+let ingredients = 
+
+
+{
+    name: { 
+        type: String, 
+        required: true, 
+        unique: [true, 'name already exists'] 
+    }, 
+    image: { 
+        type: String, 
+        required: true, 
+    },
+    cost: { 
+        type: String, 
+        enum: ['low', 'medium', 'high'], 
+        required: true 
+    }, 
+    purpose: { 
+        type: String, 
+        enum: ['moisturizing', 'repairing', 'sun protection', 'refreshing', 'anti-aging', 'purifying', 'perfuming', 'exfoliating'], 
+        required: true 
+    }
+}, 
+{ 
+    timestamps: true 
+},
+
+
 
 module.exports = mongoose.model('Ingredient', ingredientSchema);
+
